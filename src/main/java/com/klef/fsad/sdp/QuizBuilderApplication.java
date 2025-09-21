5601a51b3a72ffb9d6a1396c04599139c92fd5cf
Ramadevi-2005 <157826221+Ramadevi-2005@users.noreply.github.com>
2025-09-21 22:53:30 +0530

    Update QuizBuilderApplication.java

--- src/main/java/com/klef/fsad/sdp/QuizBuilderApplication.java
+++ src/main/java/com/klef/fsad/sdp/QuizBuilderApplication.java
@@ -2,12 +2,19 @@ package com.klef.fsad.sdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QuizBuilderApplication {
+public class QuizBuilderApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuizBuilderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuizBuilderApplication.class, args);
+    }
 
+    // ✅ Required for WAR deployment
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(QuizBuilderApplication.class);
+    }
 }
